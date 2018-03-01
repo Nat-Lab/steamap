9481d1e8843fa4ac722715294276adf8a3641ff4
Maho Morichika <nat@magicnat.com>
2018-02-28 19:50:04 -0500

    fix getInfo.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 api_key = localStorage.steamApiKey ? localStorage.steamApiKey : prompt('Steam API key (see http://steamcommunity.com/dev/apikey)');
 localStorage.steamApiKey = api_key;
 
+// https://stackoverflow.com/questions/8495687/split-array-into-chunks
+// too lazy.
+Object.defineProperty(Array.prototype, 'chunk', {
+  value: function(chunkSize) {
+    var R = [];
+    for (var i=0; i<this.length; i+=chunkSize)
+      R.push(this.slice(i,i+chunkSize));
+    return R;
+  }
+});
+
 var getFriends = id => new Promise((res, rej) => {
 
   var api_pre = 'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=' + api_key + '&relationship=friend&steamid=';
@@ -21,18 +32,27 @@ var getFriends = id => new Promise((res, rej) => {
 });
 
 var getInfoByIds = ids => new Promise((res, rej) => {
-  var prof_url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + api_key + '&steamids=' + ids.join();
-  var xhr = new XMLHttpRequest();
 
-  xhr.open('GET', prof_url);
-  xhr.onload = function () {
-    if (this.status == 200) {
-      var infos = JSON.parse(xhr.response).response.players;
-      res(infos);
-    } else rej(xhr.statusText);
-  }
+  var fetchOnce = ids => new Promise((res, rej) => {
+    var prof_url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + api_key + '&steamids=' + ids;
+    var xhr = new XMLHttpRequest();
 
-  xhr.send();
+    xhr.open('GET', prof_url);
+    xhr.onload = function () {
+      if (this.status == 200) {
+        var infos = JSON.parse(xhr.response).response.players;
+        res(infos);
+      } else rej(xhr.statusText);
+    }
+    xhr.send();
+  });
+
+  Promise.all(
+    ids.chunk(100)
+       .map(arr => arr.join())
+       .map(async ids => await fetchOnce(ids))
+  ).then(arr => res(arr.reduce((accr, cuur) => accr.concat(cuur))));
+
 });
 
 var container = document.getElementById('display');
